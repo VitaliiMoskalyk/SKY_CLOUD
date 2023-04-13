f3912b4c93978e77c115aef34c5f7d95c0b3d5ab
VitaliiMoskalyk <vitalikmoskalik@gmail.com>
2023-04-13 23:58:41 +0300

    contact + faq sections. install countup.js
    added some stiles and basic css of this pages. Little fix another code

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,22 @@
 import * as flsFunctions from "./modules/functions.js";
 import ModalOpener from "./modules/modal.js";
+import { CountUp } from "countup.js";
 
 flsFunctions.isWebP();
 
 ModalOpener();
+
+window.onload = function () {
+  const numb1 = document.getElementById("num1");
+  const numb2 = document.getElementById("num2");
+  var countUp = new CountUp("num1", numb1.innerText, {
+    enableScrollSpy: true,
+    // scrollSpyOnce,
+  });
+  var countUp2 = new CountUp("num2", numb2.innerText, {
+    enableScrollSpy: true,
+    // scrollSpyOnce,
+  });
+  countUp.start();
+  countUp2.start();
+};
